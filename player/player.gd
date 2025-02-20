79eed525828afa648b3f23ae7188cf308ab490c7
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2025-02-19 20:29:50 -0500

    integrated animation to play with inputs

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
 @export var speed := 500.0
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	print("Hello World!")
 
 func _process(delta: float) -> void:
-	velocity.x = Input.get_axis("ui_left", "ui_right") * speed
+
+	var horizontal = Input.get_axis("ui_left", "ui_right")
+	velocity.x = horizontal * speed
+	
+	# overall velocity is checked just in case we need vertical movement
+	if (velocity.length() >= 0.1):
+		sprite.play("walking")
+		sprite.flip_h = horizontal < 0
+	else:
+		sprite.play("idle")
 	
 	move_and_slide()
