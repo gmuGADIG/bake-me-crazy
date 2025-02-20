8e2c31fd0a2f30915d278d0c5ffac9734cdb7962
CTaylor <Rubiksmaster2021@gmail.com>
2025-02-19 21:26:14 -0500

    character select
    implemented base functions for character select

---
+++ character_select/character_select.gd
@@ -0,0 +1,40 @@
+extends Control
+var charsel: String
+@export var selectChar: int
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#button 1
+func _on_button_button_up() -> void:
+	selectChar = 1
+	pass # Replace with function body.
+
+func _on_button_2_button_up() -> void:
+	selectChar = 2
+	pass # Replace with function body.
+
+
+func _on_button_3_button_up() -> void:
+	selectChar = 3
+	pass # Replace with function body.
+
+
+func _on_button_4_button_up() -> void:
+	selectChar = 4
+	pass # Replace with function body.
+
+
+func _on_button_5_button_up() -> void:
+	selectChar = 5
+	pass # Replace with function body.
+
+
+func _on_button_6_button_up() -> void:
+	selectChar = 6
+	pass # Replace with function body.
