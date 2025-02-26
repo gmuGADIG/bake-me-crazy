06435f33c9c88882b50d1a44be0972e9be260371
Michael Campbell <rainbow@portal2d.xyz>
2025-02-25 19:51:50 -0500

    refactor: use input actions
    feat: input actions

--- free_roam/player/player.gd
+++ free_roam/player/player.gd
@@ -3,5 +3,5 @@ extends PathFollow2D
 @export var speed := 500.0
 
 func _process(delta: float) -> void:
-	progress += Input.get_axis("ui_left", "ui_right") * speed * delta
+	progress += Input.get_axis("move_left", "move_right") * speed * delta
 	
