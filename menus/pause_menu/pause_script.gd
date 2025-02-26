b6f8406f1dc20edc266c271fdcaa84c53a97d809
Michael Campbell <rainbow@portal2d.xyz>
2025-02-25 20:35:47 -0500

    fix: suppress unused parameter warning

--- menus/pause_menu/pause_script.gd
+++ menus/pause_menu/pause_script.gd
@@ -1,6 +1,6 @@
 extends Control
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		$PauseContainer.visible = get_tree().paused;
 		get_tree().paused = not get_tree().paused;
