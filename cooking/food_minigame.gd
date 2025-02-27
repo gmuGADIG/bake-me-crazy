5d78eb187cbcd2105929b81dda42ec43b8655075
ben <bwall4@gmu.edu>
2025-02-26 20:29:38 -0500

    Add a barebones FoodStep system.

--- cooking/food_minigame.gd
+++ cooking/food_minigame.gd
@@ -2,7 +2,43 @@ extends Node2D
 ## Base class to control food minigame stepping logic.
 class_name FoodMinigame
 
+var steps: Array[FoodStep] = []
+var step_ptr: int = 0
+
+func next_step(ingredient: FoodItem) -> void:
+	if step_ptr < steps.size():
+		# If we have a previous step, disable it.
+		if step_ptr > 0:
+			var prev: FoodStep = steps[step_ptr]
+			prev.process_mode = Node.PROCESS_MODE_DISABLED
+		
+		var next: FoodStep = steps[step_ptr]
+		step_ptr += 1
+		next.finished.connect(step_finished)
+		
+		# Enable the step then call its start function.
+		next.process_mode = Node.PROCESS_MODE_INHERIT
+		next.start()
+		
+		if ingredient != null:
+			# If we have an ingredient, reparent it to the new step.
+			ingredient.reparent(next)
+
+func step_finished(score: float, ingredient: FoodItem) -> void:
+	# When we finish the last step, move on to the next one.
+	next_step(ingredient)
+
 func _ready() -> void:
-	pass
+	# Populate the steps with each FoodStep child.
+	for child in get_children():
+		if child is FoodStep:
+			steps.append(child)
+			
+			# All steps that are not the current step are disabled.
+			child.process_mode = Node.PROCESS_MODE_DISABLED
+			
+	# Start the first step.
+	next_step(null)
+			
 	
 	
--- cooking/food_step.gd
+++ cooking/food_step.gd
@@ -2,6 +2,16 @@ extends Node
 ## Represents a single step in the recipe.
 class_name FoodStep
 
+## Adds a new ingredient to the scene.
+##
+## TODO: This should probably take something other than a Texture2D.
+func add_ingredient(tex: Texture2D) -> FoodItem:
+	var ingredient := FoodItem.create_textured_food(tex)
+	
+	add_child(ingredient)
+	
+	return ingredient
+
 ## This function will be called by the FoodMinigame when this step begins.
 func start():
 	pass
