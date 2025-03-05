fd55e8c9d8906de89f4947ea8e43b63d802f840a
Justin1L8 <justin.w.langdon@gmail.com>
2025-03-05 17:08:09 -0500

    Moved selected character to save system

--- menus/character_select/character_select.gd
+++ menus/character_select/character_select.gd
@@ -1,8 +1,5 @@
 class_name CharSelect extends Control
 
-## A number from 0 to 5 representing the currently selected character. Defaults to zero.
-var selected_character: int
-
 @onready var button_container: HBoxContainer = $ButtonHBoxContainer
 
 func _ready() -> void:
@@ -12,6 +9,6 @@ func _ready() -> void:
 		)
 
 func select_character(char_id: int) -> void:
-	selected_character = char_id
-	print("selected character: ", selected_character)
+	PlayerData.data.selected_character = char_id
+	print("selected character: ", char_id)
 	# TODO: when a character is selected, go to a new scene
--- save/save_template.gd
+++ save/save_template.gd
@@ -1,7 +1,7 @@
 class_name SaveTemplate extends Resource
 
-##This is the script/resource that stores all data pertaining to the player, and saved progress.
-##When the game is saved, all variables are stored to disk, and can be reloaded.
+## This is the script/resource that stores all data pertaining to the player, and saved progress.
+## When the game is saved, all variables are stored to disk, and can be reloaded.
 
 
 enum DayPhase {
@@ -13,20 +13,19 @@ enum DayPhase {
 	HOME
 }
 
-@export_range(1,14) var day : int
+# NOTE:
+# to save something between sessions, add a new variable here for it.
+# It MUST be an @export variable, or it won't save.
+# If anything has complicated functionality, put the logic elsewhere to keep this file organized.
+
+#- Day Phase -#
+@export_range(1,14) var day : int = 1 ## Current day, from 1 to 14
 @export var day_phase : DayPhase
+
+#- Recipes -#
+
+#- Inventory / Items -#
 @export var money : int
 
-##TODO: Recipes data
-
-##TODO: Inventory/Items
-
-##TODO: CharacterData?
-	#name
-	#hearts
-	#DateSchedule
-	#texts [
-		#"afashfiasufhaieughaseig",
-		#"hi",
-		#"you bake be crazy"
-	#]
+#- Character Data -#
+@export var selected_character: int ## Currently selected character, from 0 to 5. Set from the character select screen.
