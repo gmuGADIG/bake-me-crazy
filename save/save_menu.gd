4f6e8037727b6a499872d508cbfa10aaa1c0d84d
Michael Campbell <rainbow@portal2d.xyz>
2025-03-05 00:07:38 -0500

    fix style

--- save/save_menu.gd
+++ save/save_menu.gd
@@ -11,7 +11,6 @@ func save_file(save_slot: String) -> void:
 	var result = ResourceSaver.save(PlayerData.data,"user://"+save_slot+".tres")
 	if result > 0:
 		push_error("Error: File did not save correctly\n"+result)
-	pass
 
 
 
@@ -26,29 +25,23 @@ func load_file(save_slot: String) -> void:
 
 func _on_save_1_pressed() -> void:
 	save_file("save1")
-	pass # Replace with function body.
 
 
 func _on_save_2_pressed() -> void:
 	save_file("save2")
-	pass # Replace with function body.
 
 
 func _on_save_3_pressed() -> void:
 	save_file("save3")
-	pass # Replace with function body.
 
 
 func _on_load_1_pressed() -> void:
 	load_file("save1")
-	pass # Replace with function body.
 
 
 func _on_load_2_pressed() -> void:
 	load_file("save2")
-	pass # Replace with function body.
 
 
 func _on_load_3_pressed() -> void:
 	load_file("save3")
-	pass # Replace with function body.
