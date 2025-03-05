731f616a82bd3c416dd13316e2d5a672c7b7dc53
Justin1L8 <justin.w.langdon@gmail.com>
2025-03-05 16:57:43 -0500

    Refactored character select
    Replaced programmer art with non-copyrighted art (from gartic).
    Refactored character_select code.
    character_select autoload wasn't set up right, so the script has a static variable now instead.

--- menus/character_select/character_select.gd
+++ menus/character_select/character_select.gd
@@ -1,49 +1,17 @@
-extends Control
-var charsel: int
-@export var activeChar: Sprite2D
-#@export var selectChar: int
-# Called when the node enters the scene tree for the first time.
+class_name CharSelect extends Control
+
+## A number from 0 to 5 representing the currently selected character. Defaults to zero.
+var selected_character: int
+
+@onready var button_container: HBoxContainer = $ButtonHBoxContainer
+
 func _ready() -> void:
-	pass # Replace with function body.
+	for button in button_container.get_children():
+		button.pressed.connect(
+			func(): select_character(button.char_id)
+		)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _on_cat_button_up() -> void:
-	char_save(1)
-	activeChar.texture = load("res://menus/character_select/characters/BetterCat.png")
-
-
-func _on_hood_button_up() -> void:
-	char_save(2)
-	activeChar.texture = load("res://menus/character_select/characters/HoodGuy.png")
-
-
-func _on_ogre_button_up() -> void:
-	char_save(3)
-	activeChar.texture = load("res://menus/character_select/characters/Ogre.png")
-
-
-func _on_people_button_up() -> void:
-	char_save(4)
-	activeChar.texture = load("res://menus/character_select/characters/People1.png")
-
-
-func _on_people_1_button_up() -> void:
-	char_save(5)
-	activeChar.texture = load("res://menus/character_select/characters/People2.png")
-
-
-func _on_skele_button_up() -> void:
-	char_save(6)
-	activeChar.texture = load("res://menus/character_select/characters/Skele.png")
-	
-func char_save(num:int):
-	var charsels = ""
-	while (num > 0):
-		charsels = str(num & 1) + charsels
-		num = num >> 1
-	charsel = int(charsels)
-	
+func select_character(char_id: int) -> void:
+	selected_character = char_id
+	print("selected character: ", selected_character)
+	# TODO: when a character is selected, go to a new scene
