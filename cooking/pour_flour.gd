a33cc532b81bf88602c430911a8bdff64cd90c27
Clancy M <Cmanatron@gmail.com>
2025-03-05 21:15:42 -0500

    Finished skeleton of pouring flour

--- cooking/pour_flour.gd
+++ cooking/pour_flour.gd
@@ -1,6 +1,7 @@
 extends FoodStep
 
 var flourInBowl = 0;
+var overBowl = false;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -11,6 +12,20 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	$FlourScale.text = str(flourInBowl)
-	if(Input.is_mouse_button_pressed(1)):
+	if(Input.is_mouse_button_pressed(1) && overBowl == true):
 		flourInBowl = flourInBowl + 1
+	
+	if(flourInBowl == 300):
+		finished.emit(3)
+
+
+
+
+
+func _on_bowl_area_body_entered(body: Node2D) -> void:
+	overBowl = true;
 	pass
+
+func _on_bowl_area_body_exited(body: Node2D) -> void:
+	overBowl = false;
+	pass # Replace with function body.
