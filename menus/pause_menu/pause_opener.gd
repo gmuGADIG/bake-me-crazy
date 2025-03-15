c208b54565085422aa2f70f0621d02f92079679f
Justin1L8 <justin.w.langdon@gmail.com>
2025-03-14 23:49:37 -0400

    Fixed and refactored pause menu
    - Made the pause menu work with the pause opener.
    - Refactored the pause menu to create and destroy itself, rather than toggling visibility.

--- menus/pause_menu/pause_opener.gd
+++ menus/pause_menu/pause_opener.gd
@@ -1,19 +1,10 @@
-extends Control
+extends Node
 
-var is_paused: bool = false
+var pause_menu: Node = null
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#if is_paused is false and escaped is pressed, creates the pause menu, sets is_paused to true
-	#this requires in the pause script that once the pause scene no longer active, that the bool returns false
-	#It may also require, that the pause is changed so that when it is disabled, that it destroys itself
+func _process(_delta: float) -> void:
+	if pause_menu != null: return # while the pause menu is open, wait for it to close (free) itself
 	
-	
-	if Input.is_action_just_pressed("pause") and not is_paused:
-		var pausescreen = load("res://menus/pause_menu/pause_menu_scene.tscn").instantiate()
-		add_child(pausescreen)
-		is_paused = true
-		
-		
-	pass
+	if Input.is_action_just_pressed("pause"):
+		pause_menu = load("res://menus/pause_menu/pause_menu_scene.tscn").instantiate()
+		add_child(pause_menu)
--- menus/pause_menu/pause_script.gd
+++ menus/pause_menu/pause_script.gd
@@ -1,35 +1,36 @@
-extends Control
+extends CanvasLayer
+
+func _ready() -> void:
+	get_tree().paused = true # this is reset to false in _on_tree_exiting
 
 func _process(_delta: float) -> void:
-	#Checks if the player just pressed the input action labeled "pause" 
-	#(which is usually defined in the Input Map in Project Settings)
+	# close when the player presses "pause" again
 	if Input.is_action_just_pressed("pause"):
-		$PauseContainer.visible = get_tree().paused;
-		get_tree().paused = not get_tree().paused;
-		
-func _on_save_game_pressed() -> void:
-	$GameSavedIcon.visible = true;
-	
+		queue_free()
 
+func _on_tree_exiting() -> void:
+	get_tree().paused = false
+
+func _on_save_game_pressed() -> void:
+	$GameSavedIcon.visible = true
+	print("TODO: save game")
+
+func _on_load_game_pressed() -> void:
+	print("TODO: load game")
 
 func _on_close_menu_pressed() -> void:
-	get_tree().paused = not get_tree().paused;
-	$PauseContainer.visible = false;
-
+	get_tree().paused = false
+	queue_free()
 
 func _on_return_main_menu_pressed() -> void:
-	$ConfirmQuitContainer.visible = true;
-	$PauseContainer.visible = false;
-
+	print("TODO: go to main menu")
 
 func _on_options_pressed() -> void:
-	print("Options Pressed");
-
+	print("TODO: open options")
 
 func _on_confirm_quit_pressed() -> void:
-	print("Game Close");
-
+	print("TODO: return to main menu")
 
 func _on_return_pause_pressed() -> void:
-	$PauseContainer.visible = true;
-	$ConfirmQuitContainer.visible = false;
+	$PauseContainer.visible = true
+	$ConfirmQuitContainer.visible = false
