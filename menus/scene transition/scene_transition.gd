6c25d2b57f6200a3f958046c4dae880a1219ea35
khalid <khalidmoosa749@gmail.com>
2025-03-19 07:46:07 -0400

    fixed issue with the cover
    it works now yay

--- menus/scene transition/scene_transition.gd
+++ menus/scene transition/scene_transition.gd
@@ -1,11 +1,13 @@
 extends CanvasLayer
 
-@export var targetScenePath = ""
+var targetScenePath = ""
 
-func move_to_scene(target: String) -> void:
+func move_to_scene() -> void:
+	print('im like hey whats up hello')
 	get_tree().change_scene_to_file(targetScenePath)
-	
+
 #call this function to move the scene!
-func change_scene() -> void:
+func change_scene(targetScene) -> void:
+	targetScenePath = targetScene
 	$AnimationPlayer.play("swipe")
-	$AnimationPlayer.play("RESET")
+	
