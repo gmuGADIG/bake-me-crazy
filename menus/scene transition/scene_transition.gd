8db308e4b08e1274b3f88a41980b10aa28816226
khalid <khalidmoosa749@gmail.com>
2025-03-19 07:47:29 -0400

    Update scene_transition.gd

--- menus/scene transition/scene_transition.gd
+++ menus/scene transition/scene_transition.gd
@@ -3,10 +3,10 @@ extends CanvasLayer
 var targetScenePath = ""
 
 func move_to_scene() -> void:
-	print('im like hey whats up hello')
 	get_tree().change_scene_to_file(targetScenePath)
 
 #call this function to move the scene!
+#SceneTransition.change_scene(pathtoscene)
 func change_scene(targetScene) -> void:
 	targetScenePath = targetScene
 	$AnimationPlayer.play("swipe")
