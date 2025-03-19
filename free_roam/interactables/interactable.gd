f762653ed8641522daf153937092524c1effdb0d
Justin1L8 <justin.w.langdon@gmail.com>
2025-03-19 13:38:54 -0400

    Added interactable which changes scene

--- free_roam/interactables/interactable.gd
+++ free_roam/interactables/interactable.gd
@@ -1,6 +1,9 @@
 class_name Interactable extends StaticBody2D
 
-##For all child classes, put any actions you want here!
-func _interact()->void:
-	#print("Interacted!")
+func _ready() -> void:
+	const NPC_LAYER = 1 << 2
+	assert(collision_layer & NPC_LAYER, "Interactable is on the wrong layer! Make sure the collision layer has the NPC layer.")
+
+## Abstract function called upon interacting. Subclasses should override this.
+func _interact() -> void:
 	pass
