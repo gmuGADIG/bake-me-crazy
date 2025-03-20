1293f802e929703894ab8d7d964b65df2511c3a0
Cian Barrett <cian.m.barrett@gmail.com>
2025-03-19 21:06:02 -0400

    Updated Relationships Manager
    Added variables for RP and Heart values for each character. Updated relationships.gd to update the variables for each character accordingly, and ensure that RP cannot be below 0.

--- dialogue/relationships.gd
+++ dialogue/relationships.gd
@@ -5,7 +5,7 @@
 extends Node2D
 #Global var for dividing relationship points to hearts
 #The variable represents how much RP becomes one heart
-const RP_TO_HEARTS = 5
+const RP_TO_HEARTS = 100
 
 #Function runs on load.
 func _ready():
@@ -20,20 +20,42 @@ func heartCheck(changes):
 	var charName = changes["variable"]
 	#newVal is the new value of the changed variable
 	var newVal = changes["new_value"]
-	#currHearts is the current number of hearts the character has
-	var currHearts = Dialogic.VAR.saltyHearts
+	#heartName is the name of the heart variable of the character who's RP changed
+	var heartName
+	#This match gets the heart name based on the character
+	#If the variable that changed was not an RP value, nothing happens
+	match charName:
+		"saltyRP":
+			heartName = "saltyHearts"
+		"sourRP":
+			heartName = "sourHearts"
+		"savoryRP":
+			heartName = "savoryHearts"
+		"sweetRP":
+			heartName = "sweetHearts"
+		_:
+			return
 	
+	#currHearts is the current number of hearts the character has
+	var currHearts = Dialogic.VAR.get_variable(heartName)
+	
+	#If the new RP is below 0, it is set to 0 and the script ends
+	if newVal < 0:
+		Dialogic.VAR.set_variable(charName, 0)
+		return
+	
+	#If the new value of RP exceeds the limit (specified by RP_TO_HEARTS), then the hearts get incremented and RP is set to 0
 	if newVal > RP_TO_HEARTS:
 		#If the character's at a date-check, then nothing happens and RP stays at max
 		if currHearts % 3 == 0 and currHearts != 0:
-			Dialogic.VAR.set_variable("saltyRP", RP_TO_HEARTS)
+			Dialogic.VAR.set_variable(charName, RP_TO_HEARTS)
 			return
 		else:
 			newVal -= (RP_TO_HEARTS + 1)
-			currHearts += 1
+			Dialogic.VAR.set_variable(heartName, (currHearts + 1))
 			print("ADDED HEART!")
-		Dialogic.VAR.set_variable("saltyRP", newVal)
-		Dialogic.VAR.set_variable("saltyHearts", currHearts)
+		Dialogic.VAR.set_variable(charName, newVal)
 		
-	print("RP:", Dialogic.VAR.saltyRP)
-	print("Hearts:", Dialogic.VAR.saltyHearts)
+	#DEBUG STATEMENTS
+	print("RP:", Dialogic.VAR.get_variable(charName))
+	print("Hearts:", currHearts)
