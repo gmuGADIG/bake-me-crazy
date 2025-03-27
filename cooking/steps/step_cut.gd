fc4a8aca471f913b20db91461dec613de272b89e
Clancy M <Cmanatron@gmail.com>
2025-03-26 21:57:35 -0400

    Still working on Cutting. Slightly more functional.

--- cooking/steps/step_cut.gd
+++ cooking/steps/step_cut.gd
@@ -11,17 +11,25 @@ func _process(delta: float) -> void:
 	$Cutting.set_point_position(1,$ToCut/CutGuide.shape.a)
 	if(Input.is_mouse_button_pressed(1)):
 		$Cutting.add_point(get_local_mouse_position());
+		$Cutting/LastCutArea.position = get_local_mouse_position();
 		
 	
 	
-			
-	
 
-
-func _on_last_cut_area_area_entered(area: Area2D) -> void:
+func _on_cut_end_area_entered(area: Area2D) -> void:
 	var inLine = 0;
 	for i in range($Cutting.get_point_count()):
-		if(Geometry2D.is_point_in_polygon($Cutting.get_point_position(i),$ToCut/CutGuide.shape)):
-			inLine+=1
-	print(inLine)
+		
+		
+		$Cutting/CutChecker.position = $Cutting.get_point_position(i)
+
+		$Cutting/CutChecker.force_shapecast_update()
+		
+		if($Cutting/CutChecker.is_colliding()):
+			#print($Cutting/CutChecker.get_collider())
+			print($Cutting/CutChecker.position)
+			inLine+=1;
 	
+	
+	print(inLine)
+	print($Cutting.get_point_count())
