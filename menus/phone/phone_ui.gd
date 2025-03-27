c3c45b76eeebf524db4cd7e35f48bdafc652501e
Mira <maclennanmira@gmail.com>
2025-03-26 20:17:40 -0400

    Womp
    Add some code

--- menus/phone/phone_ui.gd
+++ menus/phone/phone_ui.gd
@@ -1,15 +1,24 @@
 extends Control
 
+var phone_opened = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	phone_opened = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if phone_opened:
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
 
 
-func _on_texture_button_pressed() -> void:
-	pass # Replace with function body.
+func _on_phone_top_pressed() -> void:
+	#play phone open animation AND pause other input
+	#TODO: Play animations
+	if phone_opened:
+		phone_opened = false
+	else:
+		phone_opened = true
