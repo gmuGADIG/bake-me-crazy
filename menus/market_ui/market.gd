ab315449a47df4d78d7ac7966dcbc2b44e086bc8
ben <bwall4@gmu.edu>
2025-04-02 22:03:19 -0400

    Basic arrow functionality

--- menus/market_ui/market.gd
+++ menus/market_ui/market.gd
@@ -4,6 +4,29 @@ extends CanvasLayer
 
 @onready var ingredient_container: FlowContainer = %IngredientContainer
 
+@onready var quantity_number := %QuantityNumber
+@onready var total_price := %TotalPrice
+
+var current_item: ItemData = null
+
+## Keeps the purchase count from going above this value. This is potentially
+## a decent idea because it means the game will never behave unpredictably.
+const ABSOLUTE_ITEM_MAX: int = 99
+
+## Gets the per-item price of the currently selected item, if there is one. If
+## no item is selected, returns 0.
+func _get_current_per_item_price() -> int:
+	if current_item == null:
+		return 0
+	return current_item.price
+	
+## Gets the current total price of the purchase. This equals the per-item price
+## times the desired number of items.
+func _get_current_purchase_price() -> int:
+	return _get_current_per_item_price() * _purchase_quantity
+
+var _purchase_quantity: int = 0
+
 func _ready() -> void:
 	var button_group = ButtonGroup.new()
 	for i in range(ingredient_container.get_child_count()):
@@ -16,6 +39,31 @@ func _ready() -> void:
 			item_button.button.pressed.connect(func():
 				select_item(items[i])
 			)
+			
+	# Initialize various displays.
+	_update_purchase_display()
+	%LeftArrow.pressed.connect(func():
+		_purchase_quantity -= 1
+		if _purchase_quantity < 0:
+			_purchase_quantity = 0
+		_update_purchase_display()
+	)
+	
+	%RightArrow.pressed.connect(func():
+		_purchase_quantity += 1
+		if _purchase_quantity > ABSOLUTE_ITEM_MAX:
+			_purchase_quantity = ABSOLUTE_ITEM_MAX
+		# TODO: Is there a maximum quantity?
+		_update_purchase_display()
+	)
+
+func _update_purchase_display():
+	quantity_number.text = str(_purchase_quantity)
+	total_price.text = str("$", _get_current_purchase_price())
 
 func select_item(item: ItemData) -> void:
 	print("selected item `%s`" % item.code_name)
+	current_item = item
+	# Must change the display when we change the item.
+	_update_purchase_display()
+	
