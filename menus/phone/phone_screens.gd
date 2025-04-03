7b856ecdc9e7d7c0d3f6ab3293fa9be071790af1
Mira <maclennanmira@gmail.com>
2025-04-02 20:07:25 -0400

    Add screen manager skeleton

---
+++ menus/phone/phone_screens.gd
@@ -0,0 +1,19 @@
+extends Control
+class_name PhoneScreen
+
+## Class to manage which screens are open
+## When a button is clicked a signal will be passed
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func close_screen() -> void:
+	pass
+	
+func open_screen() -> void:
+	pass
