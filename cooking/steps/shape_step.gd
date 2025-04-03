77b2e0be35e745a391bc329cb997c5dc42b39f12
TheRedEncryption <64206126+TheRedEncryption@users.noreply.github.com>
2025-04-02 21:06:32 -0400

    started work on implementing spinny

--- cooking/steps/shape_step.gd
+++ cooking/steps/shape_step.gd
@@ -1,10 +1,11 @@
 extends FoodStep
 class_name ShapeStep
 
+enum {PENDING, IN_PROGRESS, FINISHED}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
