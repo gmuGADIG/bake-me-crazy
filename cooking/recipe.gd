8ee50b254cb3c83e7a723b81c8012112fbc0a3cb
Justin Langdon <justin.w.langdon@gmail.com>
2025-04-09 18:30:52 -0400

    Improved recipe book, implemented selecting recipes

--- cooking/recipe.gd
+++ cooking/recipe.gd
@@ -1,15 +1,6 @@
-extends Resource
-
-class_name Recipe # recipe class
+class_name Recipe extends Resource
 
 @export var name = ""
 @export var food_image: Texture2D
 @export_multiline var instructions = ""
 @export var is_locked = false
-
-# Check to see if recipes are locked
-func is_recipe_locked():
-	if (is_locked == true):
-		print("oh shoot a recipe")
-	else:
-		print("recipe is locked")
