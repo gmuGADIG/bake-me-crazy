175d38828a0a2fee6c399808abebb2b69e6f6a57
Mira <maclennanmira@gmail.com>
2025-04-16 21:38:06 -0400

    Create Inventory UI
    -Modified ItemData to have a description
    -Created UI for Inventory in phone scene
    -And the code! (WIP)

--- Item_Data.gd
+++ Item_Data.gd
@@ -2,11 +2,13 @@ class_name ItemData extends Resource
 
 #Name of the item
 @export var name: String
+@export var desription:String
 #If an item has no quality attribute use a negative integer
 @export var quality: int = -1
 #AtlasTexture??
 @export var icon: Texture2D
 #Price of the item for buying
 @export var price: int = 0
+
 
 ###TODO: If the item is bakable add a recipe: @export var recipe: Recipe
