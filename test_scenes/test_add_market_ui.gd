467eb3888dea3773b77e59eb50d2505bb184f398
ben <bwall4@gmu.edu>
2025-04-16 21:24:36 -0400

    Delete test scene transition script

--- test_scenes/test_add_market_ui.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_page_down"):
-		# Instead of loading a UI, simply reload the current scene to test
-		# that the is_mid_interaction() logic works.
-		SceneTransition.change_scene("res://free_roam/world/streets/streets.tscn")
