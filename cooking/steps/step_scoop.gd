8bd7c9ac3bbc8e42455e8c67311688c942a4e80d
Cian Barrett <cian.m.barrett@gmail.com>
2025-04-16 21:31:50 -0400

    Spawns dough
    Added code to spawn the dough when the bowl is clicked, then the environment is clicked.

--- cooking/steps/step_scoop.gd
+++ cooking/steps/step_scoop.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var gameTimer: Label = $gameTimer
 @onready var bowl: Area2D = $Bowl
+@onready var place_pos: Area2D = $placePos
 
 #Time limit
 var timeLimit = 15
@@ -22,6 +23,7 @@ func _process(delta: float) -> void:
 			timeLimit -= delta
 		else:
 			timeLimit = 0.001
+		
 		gameTimer.text = "Time left: %.2f" % timeLimit
 
 
@@ -33,7 +35,29 @@ func _input(event: InputEvent) -> void:
 	if event.is_action("interact") && mouseAtBowl == true && isScooped == false:
 		isScooped = true;
 		print("Scooped!")
+	elif event.is_action("interact") && mouseAtBowl == false && isScooped == true:
+		print("Placed scoop!")
+		isScooped = false;
 
+		
+		#Spawns cookie dough
+		#Art-stuff
+		var doughSprite = Sprite2D.new()
+		var texture = load("res://temp_art/gartic/forg.png")
+		doughSprite.position = get_local_mouse_position()
+		doughSprite.set_texture(texture)
+		add_child(doughSprite)
+		#Collision-stuff
+		var doughCollision = CollisionShape2D.new()
+		doughCollision.position = get_local_mouse_position()
+		doughCollision.shape = CircleShape2D.new()
+		doughCollision.shape.radius = 35
+		add_child(doughCollision)
+		
+		"""
+		print("placePos: ", place_pos.position)
+		print("doughCollision: ", doughCollision.position)
+		"""
 
 #Signals to detect whether the mouse's position is at the bowl or not
 #Used to help determine if cookie dough can be scooped
@@ -45,3 +69,7 @@ func _on_bowl_mouse_exited() -> void:
 	print("Exited")
 	mouseAtBowl = false
 	
+
+
+func _on_place_pos_body_entered(body: Node2D) -> void:
+	print("true")
