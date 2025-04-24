a79ee50ff1ba6f5ebbebf51069e87fe010f1b8c1
ben <bwall4@gmu.edu>
2025-04-23 21:14:52 -0400

    Recipe book variant columns

--- cooking/recipe_variant.gd
+++ cooking/recipe_variant.gd
@@ -3,3 +3,5 @@ class_name RecipeVariant extends Resource
 @export var texture: Texture2D = null
 @export var key_ingredient: StringName = ""
 @export var key_ingredient_requirement: int = 0
+
+@export var name: String
--- menus/recipe_book/recipe_book_page.gd
+++ menus/recipe_book/recipe_book_page.gd
@@ -4,6 +4,13 @@ class_name RecipeBookPage extends Panel
 @onready var food_name: Label = %FoodName
 @onready var food_desc: Label = %FoodDesc
 
+@onready var variants_left = %FoodVariantsLeft
+@onready var variants_right = %FoodVariantsRight
+
+func populate_variant(idx: int, text: String) -> void:
+	var column: Label = variants_left if idx % 2 == 0 else variants_right
+	column.text += str("- ", text, "\n")
+
 func display_recipe(recipe: Recipe, is_selected: bool) -> void:
 	food_name.text = recipe.name
 	food_desc.text = recipe.instructions
@@ -12,5 +19,10 @@ func display_recipe(recipe: Recipe, is_selected: bool) -> void:
 		push_error("Recipe doesn't have any variants!")
 		return
 	food_icon.texture = recipe.variants[0].texture
+	
+	variants_left.text = ""
+	variants_right.text = ""
+	for i in range(0, recipe.variants.size()):
+		populate_variant(i, recipe.variants[i].name)
 	
 	modulate.a = 0.5 if is_selected else 1.0
