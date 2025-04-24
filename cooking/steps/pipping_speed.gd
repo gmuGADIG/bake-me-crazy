ed4b8078e53fb308725b38fbe8e3474e0b58958d
Carlin Jasper <cjasper@gmu.edu>
2025-04-23 20:25:30 -0400

    Pipping speed working
    I don't exactly know what's expected but it tracks the mouse Y velocity and has variables for tracking fast, slow and just right movements.

--- cooking/steps/pipping_speed.gd
+++ cooking/steps/pipping_speed.gd
@@ -7,21 +7,54 @@ extends FoodStep
 @onready var speed_text := $Label
 @onready var slider := $HSlider
 
+var mouse_over : bool 
+var pipping: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+var slowPoints:float
+var rightPoints: float
+var fastPoints: float
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var speed :float = abs(Input.get_last_mouse_velocity().x)
-	if speed < 10:
-		speed_text.text = "NOT MOVING"
-	elif speed < slow_threshold:
-		speed_text.text = "SLOW"
-	elif speed < mid_threshold:
-		speed_text.text = "JUST RIGHT"
-	elif speed < fast_threshold:
-		speed_text.text = "FAST"
-	slider.value = speed
+	if mouse_over and Input.is_action_just_pressed("interact"):
+		pipping = true
+		slowPoints = 0
+		rightPoints = 0
+		fastPoints = 0
+	if Input.is_action_just_released("interact"):
+		pipping = false
+		var totalPoints: float = slowPoints + rightPoints + fastPoints
+		print("Slow: " + str(slowPoints))
+		print("RIGHT: " + str(rightPoints))
+		print("Fast: " + str(fastPoints))
+		var right_ratio := rightPoints/totalPoints
+		print("RIGHT RATIO: " + str(right_ratio))
+	if pipping:
+		var speed :float = abs(Input.get_last_mouse_velocity().y)
+		if speed < 10:
+			speed_text.text = "NOT MOVING"
+		elif speed < slow_threshold:
+			speed_text.text = "SLOW"
+			slowPoints += 1
+		elif speed < mid_threshold:
+			speed_text.text = "JUST RIGHT"
+			rightPoints += 1
+		elif speed < fast_threshold:
+			speed_text.text = "FAST"
+			fastPoints += 1
+		slider.value = speed
 	pass
+
+
+func _on_character_body_2d_mouse_entered() -> void:
+	print("Mouse over")
+	mouse_over = true
+	pass # Replace with function body.
+
+
+func _on_character_body_2d_mouse_exited() -> void:
+	mouse_over = false
+	pass # Replace with function body.
