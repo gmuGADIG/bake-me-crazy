ec68c7922561f2e3a0c7daf04ce32bd4fc083ca2
Justin1L8 <justin.w.langdon@gmail.com>
2025-04-26 16:53:01 -0400

    Finished inventory item design, reworked shop to work with it
    Data is stored in the ItemData (FoodData & IngredientData) types, created as resources saved to files.
    Instances are ItemInstance's, which reference the data.
    Still needed: phone and recipe book need to work with this too

---
+++ items/recipe.gd
@@ -0,0 +1,5 @@
+class_name FoodGroup extends Resource
+
+@export var display_name = ""
+@export_multiline var instructions = ""
+@export var variants: Array[FoodData]
--- test_scenes/item_info.gd
+++ test_scenes/item_info.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 	pass
 
 #Applies item data and quantity to control nodes.
-func set_item_slot_data(item_data: ItemData) -> void:
+func set_item_slot_data(item_data: InventoryItem) -> void:
 	item_texture.texture = item_data.icon
 	item_label.text = item_data.name
 	quantity_label.text = InventorySystem.player_inventory[item_data]
