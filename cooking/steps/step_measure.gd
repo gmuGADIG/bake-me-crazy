c519f974af776d2d49e15c53e81fbaee8962b97f
Michael Campbell <rainbow@portal2d.xyz>
2025-05-02 21:16:26 -0400

    Revert "Merge pull request #99 from gmuGADIG/flour_art"
    This reverts commit 561a7756a5c86eda79f17be5fd08a4d25d3fceb6, reversing
    changes made to ed15578f3ca85b0bcb1737bdf6bc040062f61613.

--- cooking/steps/step_measure.gd
+++ cooking/steps/step_measure.gd
@@ -10,35 +10,19 @@ var flour_in_bowl: float = 0
 var done := false
 
 @onready var bowl_area: Area2D = %BowlArea
-@onready var flour_pourer: CharacterBody2D = %FlourPourer
-
-@onready var hand_open = load("res://cooking/cooking_art/Minigame_HandCursorPoint.png")
-@onready var hand_closed = load("res://cooking/cooking_art/Minigame_HandCursorGrab.png")
+@onready var flour_pourer: DraggableObject = %FlourPourer
 
 func _process(delta: float) -> void:
 	if done: return
-	
-	flour_pourer.position = get_global_mouse_position();
-	
-	#var bool mouseDown = 
-	
-	if(Input.is_mouse_button_pressed(1)):
-		$FlourPourer/HandSprite.texture = hand_closed
-		$FlourPourer/HandSprite/Powder.emitting = true;
-	else:
-		$FlourPourer/HandSprite.texture = hand_open
-		$FlourPourer/HandSprite/Powder.emitting = false;
-		
-	
 
 	var in_bowl_area = bowl_area.overlaps_body(flour_pourer)
 	
 	$FlourScale.text = "%.1f" % flour_in_bowl
 	
-	if Input.is_mouse_button_pressed(1) && in_bowl_area:
+	if in_bowl_area:
 		flour_in_bowl = flour_in_bowl + delta * pour_speed
 	
-	if flour_in_bowl >= target_amount and not Input.is_mouse_button_pressed(1):
+	if flour_in_bowl >= target_amount and not in_bowl_area:
 		var score := remap(flour_in_bowl, target_amount + pour_speed * .5, target_amount + pour_speed * 1.5, 3, 0)
 		score = clampf(score, 0, 3)
 		finished.emit(score)
