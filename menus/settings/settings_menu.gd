9d44b2e8547e901619412223082138184c80e900
ben <bwall4@gmu.edu>
2025-05-29 22:00:41 -0400

    settings menu: open/close animation

--- menus/settings/settings_menu.gd
+++ menus/settings/settings_menu.gd
@@ -6,6 +6,21 @@ extends Control
 var master_bus := AudioServer.get_bus_index("Master")
 var music_bus := AudioServer.get_bus_index("Music")
 var sfx_bus := AudioServer.get_bus_index("SFX")
+
+const BOTTOM_Y = 660
+var open_close_tween: Tween = null
+
+func animate_open_close(open: bool):
+	if open_close_tween:
+		open_close_tween.kill()
+	open_close_tween = create_tween()
+	self.position.y = BOTTOM_Y if open else 0
+	open_close_tween.tween_property(self, "position:y", 0 if open else BOTTOM_Y, 0.3)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
+	if not open:
+		open_close_tween.tween_callback(queue_free)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#Get current vol settings on load and set them (I just made place holders to test)
@@ -15,6 +30,8 @@ func _ready() -> void:
 	AudioServer.set_bus_volume_db(master_bus, linear_to_db(master_slider.value))
 	AudioServer.set_bus_volume_db(music_bus, linear_to_db(music_slider.value))
 	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(sfx_slider.value))
+	
+	animate_open_close(true)
 	pass # Replace with function body.
 
 
@@ -39,5 +56,5 @@ func _on_sfx_vol_slider_value_changed(value: float) -> void:
 
 
 func _on_back_button_pressed() -> void:
-	queue_free()
+	animate_open_close(false)
 	
