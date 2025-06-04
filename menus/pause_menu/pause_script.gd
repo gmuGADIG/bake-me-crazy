1408bac0e2c75caec66b99f451b411055f310e90
ben <bwall4@gmu.edu>
2025-06-04 19:49:03 -0400

    Somewhat decent pause menu animation

--- menus/pause_menu/pause_script.gd
+++ menus/pause_menu/pause_script.gd
@@ -4,9 +4,24 @@ extends CanvasLayer
 
 var submenu: Control = null # the currently open sub-menu, e.g. the saves or options menu
 
+@onready var buttons = [
+	%OpenSaves,
+	%CloseMenu,
+	%ReturnMainMenu,
+	%Options
+]
+var button_pos_y = []
+
 func _ready() -> void:
 	get_tree().paused = true # this is reset to false in _on_tree_exiting
 	MainMusicPlayer.set_volume(0.3)
+	
+	for button in buttons:
+		button_pos_y.push_back(button.position.y)
+	
+# for animating the panel scale
+var scale_theta: float = 0.0
+@onready var center_panel := %CenterPanel
 
 func _process(_delta: float) -> void:
 	if submenu != null: return # wait for the sub-menu to close first
@@ -14,6 +29,14 @@ func _process(_delta: float) -> void:
 	# close when the player presses "pause" again
 	if Input.is_action_just_pressed("pause"):
 		queue_free()
+		
+	scale_theta += TAU * _delta * 0.7
+	scale_theta = fmod(scale_theta, TAU)
+	center_panel.scale.y = 1 + 0.01 * sin(scale_theta)
+	center_panel.scale.x = 1 - 0.01 * cos(scale_theta)
+	
+	#for i in range(0, buttons.size()):
+		#buttons[i].position.y = button_pos_y[i] + 1 * sin(scale_theta + i)
 
 func open_submenu(submenu_scene: PackedScene) -> void:
 	submenu = submenu_scene.instantiate()
