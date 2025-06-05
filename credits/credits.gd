af29b88dd3c58e908ab46c0838971267014d9930
Michael Campbell <rainbow@portal2d.xyz>
2025-06-04 21:36:50 -0400

    credits prototype

---
+++ credits/credits.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@export var start_pause := 1.5
+
+@onready var background: ScrollingBackground = %Background
+@onready var scroller: Scroller = %Scroller
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(start_pause, false).timeout
+	background.started = true
+	scroller.started = true
