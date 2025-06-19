df85f75d541b5f22863d5b1cf4b1b079666d7af0
Michael Campbell <me@micycle.dev>
2025-06-18 20:30:56 -0400

    step results is now in front of the screen
    you no longer automatically buy 10 vanillas

--- save/player_data.gd
+++ save/player_data.gd
@@ -5,31 +5,12 @@ extends Node
 var data := SaveTemplate.new()
 
 func _ready() -> void:
-	Inventory.add_item(load("res://items/foods/cake_vanilla.tres"), 3)
-	Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	var shop_item : ShopItem = ShopItem.new()
-	shop_item.price = 0
-	shop_item.quality = 3
-	shop_item.data = load("res://items/ingredients/vanilla.tres")
-	Inventory.buy_item(shop_item,10)
+	#Inventory.add_item(load("res://items/foods/cake_vanilla.tres"), 3)
 	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
-	#Inventory.add_item(load("res://items/foods/cake_chocolate.tres"), 2)
+	
+	pass
 
 func load_file(save_template : SaveTemplate) -> void:
 	data = save_template
 	SceneTransition.change_scene_to_file(data.scene_path)
 	Dialogic.load_full_state(data.dialogic_blob)
-	# TODO: include the current scene in the save and go there
