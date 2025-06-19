1d3343cd02a995748a96b79fc738cbf458c18265
ben <bwall4@gmu.edu>
2025-06-18 20:27:16 -0400

    Change InstructionsPanel to use Cookbook art, enabling flour pour

--- cooking/steps/instructions_panel.gd
+++ cooking/steps/instructions_panel.gd
@@ -1,41 +1,40 @@
 @tool
 extends Node
 
-static var seen: Dictionary = {}
+# TODO: Decide whether we use this or not.
+# - The pour_floor minigame always needs to show the recipe so that we can
+#   see how many grams to pour.
+# - We could also add a dismiss button.
+# - 
+# static var seen: Dictionary = {}
 
-@onready var title_box: Label = $Title
-@onready var text_box : Label = $Title/InstructionsText
+@onready var title_box: RichTextLabel = $TaskTitle
+@onready var text_box : RichTextLabel = $Instructions
 
 @export var minigame_title:String: set = set_title
-@export var instructions:String: set = set_instruct
+@export_multiline var instructions:String: set = set_instruct
 
 func parent_name() -> String:
 	return get_parent().name
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if parent_name() in seen and not Engine.is_editor_hint():
-		queue_free()
+	# if parent_name() in seen and not Engine.is_editor_hint():
+	# 	queue_free()
 	
-	seen[parent_name()] = null
+	# seen[parent_name()] = null
 
 	title_box.text = minigame_title
 	text_box.text = instructions
+	
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func set_instruct(new_value: String) ->void:
 	instructions = new_value
-	if(text_box != null):
+	if text_box != null:
 		text_box.text = instructions
-	pass
 	
 func set_title(new_value: String) -> void:
 	minigame_title = new_value
-	if(title_box != null):
-		title_box.text = minigame_title
-	pass
+	if title_box != null:
+		title_box.text = str("[wave freq=5]", minigame_title, "[/wave]")
