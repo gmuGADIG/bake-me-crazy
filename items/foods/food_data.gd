5fa648231923412f717523e3caae6a4935fe79ad
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2025-07-03 14:27:44 -0400

    Add enum for foodType in food_data
    This change feels majorly scuffed, but it was the best way that I could conceive detecting the type of food being done.
    I was originally trying to hack it using the recipe display name, but it would have immediately broken if someone changed a name.
    The enum is technically a duplicate of the one from FoodItem, but FoodItem it completely unused outside of lunch rush.

--- items/foods/food_data.gd
+++ items/foods/food_data.gd
@@ -1,6 +1,23 @@
 class_name FoodData extends ItemData
 
+enum FoodType {
+	NULL, ##Default value that shouldn't be used
+	CAKE,
+	CROISSANT,
+	CANNELLE,
+	PUFF_ROLL,
+	SCONE,
+	TART,
+	COOKIE,
+	BREAD,
+	BAR,
+	BROWNIE,
+	MARACON,
+	SWEET_ROLL
+}
+
 @export var cooking_minigame: PackedScene
+@export var food_type : FoodType
 
 ## The ingredient used. Determines the cost to make, and the flavor profile.
 ## May be null for no key ingredient (e.g. sourdough bread, plain cake, etc.)
