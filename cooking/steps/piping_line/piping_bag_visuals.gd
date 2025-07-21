b1ec790ff7bc55c2952c4ebef5f4b2012ee9162d
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2025-07-20 20:05:12 -0400

    Icing Trail
    Draws a trail of icing, with thickness depending on the speed the player drags

--- cooking/steps/piping_line/piping_bag_visuals.gd
+++ cooking/steps/piping_line/piping_bag_visuals.gd
@@ -1,11 +1,38 @@
 extends Node2D
 
+@export var lift_height : float = 100 ## How high above the path follow the icing bag is lifted when idling
+
 ##The amount of distance the mask needs to travel to go from 100% full to 0% full
 #@export var piping_mask_travel_delta : float = 285.0
 @onready var icing_mask_height : float = $IcingMasks/FullMask.texture.height
 
+func _ready() -> void:
+	self.position = $Icing.path_follow.global_position-Vector2(0,lift_height)
+
+func set_icing_color(ingredient : IngredientData.IngredientType) -> void:
+	var icing_color : Color
+	match ingredient:
+		IngredientData.IngredientType.CINNAMON:
+			icing_color = Color.SIENNA
+		IngredientData.IngredientType.ORANGE:
+			icing_color = Color.ORANGE
+		IngredientData.IngredientType.PISTACHIO:
+			icing_color = Color.WEB_GREEN
+		IngredientData.IngredientType.PORK:
+			icing_color = Color.SADDLE_BROWN
+		IngredientData.IngredientType.CHEESE:
+			icing_color = Color.LIGHT_GREEN
+		
+	$Sprites/PipingBagFull.modulate = icing_color
+	$Icing/Visual.default_color = icing_color
+
+func is_lowered() -> bool:
+	return absf(global_position.y- $Icing.path_follow.global_position.y) < 10
+
 ##Expects a percentage, between 100.0% and 0.0%, and displays the filling accordingly
-func display_icing(percentage_filled : float) -> void:
+func display_piping_bag(percentage_filled : float) -> void:
+	$Icing.path_follow.progress_ratio = percentage_filled/100.0
+	self.global_position.x =  $Icing.path_follow.global_position.x
 	var mask_lerp_dist : float = clampf(percentage_filled/100.0,0.0,1.0)
 	var mask_texture_size : float = lerpf(icing_mask_height,0,mask_lerp_dist)
 	$IcingMasks/FullMask.texture.height = max(mask_texture_size,1)
@@ -13,3 +40,12 @@ func display_icing(percentage_filled : float) -> void:
 	var offset : float = icing_mask_height-mask_texture_size
 	$IcingMasks/FullMask.offset.y = 0.5*offset
 	$IcingMasks/FullMask/FadeMask.offset.y = offset
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("minigame_interact"):
+		if event.is_pressed():
+			var tween : Tween = get_tree().create_tween()
+			tween.tween_property(self,"position:y", $Icing.path_follow.global_position.y,0.25).set_trans(Tween.TRANS_QUAD)
+		elif event.is_released():
+			var tween : Tween = get_tree().create_tween()
+			tween.tween_property(self,"position:y", $Icing.path_follow.global_position.y-lift_height,0.25).set_trans(Tween.TRANS_QUAD)
--- cooking/steps/piping_line/piping_line_minigame.gd
+++ cooking/steps/piping_line/piping_line_minigame.gd
@@ -1,8 +1,8 @@
 extends FoodStep
 
-@export var percent_piped_per_pixel_per_second : float = 10
-@export var piping_bag_idle_pos : Vector2
-@export var piping_bag_using_pos : Vector2
+
+
+@export var percent_piped_per_pixel : float = 10
 
 @export var target_piping_speed : float = 0.5
 @export var target_tolerance : float = 0.2
@@ -10,17 +10,14 @@ extends FoodStep
 var num_in_target : int
 var num_out_of_target : int
 
-@onready var full_puff_roll_sprite : Sprite2D = $PuffRoll/Sprites/PastryFull
-@onready var glazed_sweet_roll_sprite : Sprite2D = $SweetRoll/Sprites/GlazedRoll
-
 var food_type : FoodData.FoodType
 var percent_piped : float = 0.0 ##Ranges between 0 and 100
 
 var last_mouse_pos : Vector2
-@export var drag_distance_average_amount : int = 10 ##The size of rolling_drag_distance needed to calculate the average drag distance. More = larger delay when first piping 
+@export var drag_sample_size_requirement : int = 10 ##The size of rolling_drag_distance needed to calculate the average drag distance. More = larger delay when first piping 
 var recent_drag_distances : Array[float]
 
-var clicking : bool
+var currently_piping : bool
 
 
 
@@ -36,133 +33,62 @@ func _ready() -> void:
 		"This step is invalid for the given food type"
 	)
 	
-	set_visuals(recipe)
-	
-	#print(MorningShift.instance.current_recipe.ingredient)
-	pass
-	#var arr : Array[float] = [1,2,3,4,5,6,7,8]
-	#var m : Material = $Glaze.material
-	#m.set_shader_parameter("values",arr)
-	#pass # Replace with function body.
+	$PipingBag.set_icing_color(recipe.ingredient.ingredient_type)
 
 
-func set_visuals(recipe : FoodData) -> void:
-	food_type = recipe.food_type
-	if food_type == FoodData.FoodType.SWEET_ROLL:
-		glazed_sweet_roll_sprite.texture = recipe.image
-		$SweetRoll.show()
-		$PuffRoll.hide()
-	elif food_type == FoodData.FoodType.PUFF_ROLL:
-		full_puff_roll_sprite.texture = recipe.image
-		$SweetRoll.show()
-		$SweetRoll.hide()
-	else:
-		assert(false,"There are no sprites for this given food type")
-
-func _input(event: InputEvent) -> void:
-	if event.is_action("minigame_interact"):
-		if event.is_pressed():
-			var tween : Tween = get_tree().create_tween()
-			tween.tween_property($PipingBag,"position",piping_bag_using_pos,0.25).set_trans(Tween.TRANS_QUAD)
+func _physics_process(delta: float) -> void:
+	if $PipingBag.is_lowered():
+		if not currently_piping:
+			currently_piping = true
 			last_mouse_pos = get_global_mouse_position()
-			clicking = true
-		elif event.is_released():
-			var tween : Tween = get_tree().create_tween()
-			tween.tween_property($PipingBag,"position",piping_bag_idle_pos,0.25).set_trans(Tween.TRANS_QUAD)
-			recent_drag_distances.clear()
-			clicking = false
-
-func _process(delta: float) -> void:
-	if clicking:
+	else:
+		currently_piping = false
+	
+	if currently_piping:
 		var new_mouse_pos : Vector2 = get_global_mouse_position()
-		var drag_dist : float = new_mouse_pos.y-last_mouse_pos.y
-		var amount_piped : float = max(drag_dist*percent_piped_per_pixel_per_second*delta,0)
+		var drag_dist : float = new_mouse_pos.x-last_mouse_pos.x
+		var amount_piped : float = max(drag_dist*percent_piped_per_pixel*delta,0)
 		percent_piped = move_toward(percent_piped,100.0,amount_piped)
-		last_mouse_pos = new_mouse_pos
 		
-		##Visual Feedback
-		$PipingBag.display_icing(percent_piped)
-		match food_type:
-			FoodData.FoodType.SWEET_ROLL:
-				$SweetRoll.display_icing(percent_piped)
-			FoodData.FoodType.PUFF_ROLL:
-				$PuffRoll.display_filling(percent_piped)
+		
+		var icing_size : PipeLineIcing.IcingSize = PipeLineIcing.IcingSize.NONE
 		
 		
 		if amount_piped > 0:
 			recent_drag_distances.append(amount_piped)
-			if recent_drag_distances.size() >= drag_distance_average_amount:
-				var average_drag_dist : float = calc_average_drag_distance()
-				$SpeedDial.display_speed(average_drag_dist,target_piping_speed,target_tolerance)
-				if absf(average_drag_dist-target_piping_speed) < target_tolerance:
-					num_in_target += 1
-				else:
-					num_out_of_target += 1
+			var is_sample_size_large_enough : bool = recent_drag_distances.size() >= drag_sample_size_requirement
+			if is_sample_size_large_enough:
+				var average_drag_dist : float = averagef(recent_drag_distances)
 				
-				prints(num_in_target,",",num_out_of_target)
+				var distance_from_target_piping_speed : float = average_drag_dist-target_piping_speed
+				if distance_from_target_piping_speed > target_tolerance:
+					num_out_of_target += 1
+					icing_size = PipeLineIcing.IcingSize.TOO_LARGE
+				elif distance_from_target_piping_speed < -target_tolerance:
+					num_out_of_target += 1
+					icing_size = PipeLineIcing.IcingSize.TOO_SMALL
+				else:
+					num_in_target += 1
+					icing_size = PipeLineIcing.IcingSize.JUST_RIGHT
+					
 				recent_drag_distances.remove_at(0)
+		
+		## Visual Feedback
+		$PipingBag.display_piping_bag(percent_piped)
+		if not icing_size == PipeLineIcing.IcingSize.NONE:
+			$PipingBag/Icing.draw_icing_line(icing_size)
+		
+		last_mouse_pos = new_mouse_pos
+	
 	
 	if is_equal_approx(percent_piped,100.0):
 		var total_data_points : float = num_in_target+num_out_of_target
-		var end_score : float = num_in_target/total_data_points
+		var end_score : float = 3.0*(num_in_target/total_data_points)
 		finished.emit(end_score)
-		print("game over")
-		
 
-func calc_average_drag_distance() -> float:
+
+func averagef(array : Array[float]) -> float:
 	var sum : float
-	for i in recent_drag_distances:
+	for i in array:
 		sum += i
-	return sum/recent_drag_distances.size()
-#var slowPoints:float
-#var rightPoints: float
-#var fastPoints: float
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#if mouse_over and Input.is_action_just_pressed("minigame_interact"):
-		#pipping = true
-		#slowPoints = 0
-		#rightPoints = 0
-		#fastPoints = 0
-	#if Input.is_action_just_released("minigame_interact"):
-		#pipping = false
-		#RemoteTransform2D
-	#if pipping and !game_done:
-		#var speed :float = Input.get_last_mouse_velocity().y
-		#if speed < 10:
-			#speed_text.text = "NOT MOVING"
-		#elif speed < slow_threshold:
-			#speed_text.text = "SLOW"
-			#slowPoints += 1
-		#elif speed < mid_threshold:
-			#speed_text.text = "JUST RIGHT"
-			#rightPoints += 1
-		#elif speed < fast_threshold:
-			#speed_text.text = "FAST"
-			#fastPoints += 1
-		#slider.value = speed
-	#pass
-#
-#
-#func _on_character_body_2d_mouse_entered() -> void:
-	#print("Mouse over")
-	#mouse_over = true
-	#pass # Replace with function body.
-#
-#
-#func _on_character_body_2d_mouse_exited() -> void:
-	#mouse_over = false
-	#pass # Replace with function body.
-#
-#func _on_end_point_mouse_entered() -> void:
-	#print("END")
-	#var totalPoints: float = slowPoints + rightPoints + fastPoints
-	#print("Slow: " + str(slowPoints))
-	#print("RIGHT: " + str(rightPoints))
-	#print("Fast: " + str(fastPoints))
-	#var right_ratio : int = (rightPoints/totalPoints) * 100
-	#print("RIGHT RATIO: " + str(right_ratio))
-	#accuracy_text.text = "Accuracy: " + str(right_ratio) + "%"
-	#game_done = true
-#
-	#finished.emit((right_ratio / 100.) * 3)
+	return sum/array.size()
