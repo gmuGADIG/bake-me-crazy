9c3c7cca3c789e792dba6a1ae0231a94e85a71f5
Justin1L8 <justin.w.langdon@gmail.com>
2025-07-23 15:31:25 -0400

    Integrate boss dialogue

--- dialogue/internal/code/break_npc/break_npc_system.gd
+++ dialogue/internal/code/break_npc/break_npc_system.gd
@@ -13,10 +13,10 @@ func prompt_gift():
 
 func start_daily_chat():
 	var current_npc = LunchBreakNPC.latest_npc
-	Dialogic.Jump.jump_to_label("day%s" % current_npc.talk_count)
-	
 	current_npc.talk_count += 1
 	current_npc.is_repeating = true
+	Dialogic.Jump.jump_to_label(str(current_npc.talk_count))
+	
 
 func compute_gift_reward(liked: bool) -> float:
 	var quality = min(3, Dialogic.VAR.read_only.gift_quality)
