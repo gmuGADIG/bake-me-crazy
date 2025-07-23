40d214ac6631fe20c214f2671c46c7ea83a9203a
ben <bwall4@gmu.edu>
2025-07-23 13:28:20 -0400

    Pour step texture scaling polish
    This is cool but difficult to do correctly. I think it is correct
    now, though.

--- cooking/steps/pour_step/single_pourer.gd
+++ cooking/steps/pour_step/single_pourer.gd
@@ -16,13 +16,34 @@ var pour_rate_multiplier := 1.0
 var done := false
 var score := 1.0
 
+# In order to support multiple texture scales for the SinglePourer sprite, we need to also
+# adjust our gameplay-relevant constant values (measured relative to the 256x256 sprite at the
+# reference scale of 0.734) to match the new texture scale.
+var tex_scale := 0.734
+const TEX_SCALE_REF := 0.734
+
+func unpoured() -> float:
+	return UNPOURED * TEX_SCALE_REF / tex_scale
+func poured() -> float:
+	return POURED * TEX_SCALE_REF / tex_scale
+func pour_perfect() -> float:
+	return POUR_PERFECT * TEX_SCALE_REF / tex_scale
+func pour_ready() -> float:
+	return POUR_READY * TEX_SCALE_REF / tex_scale	
+
 func set_container_texture(texture: Texture2D) -> void:
 	%SinglePourer.texture = texture
+	
+	tex_scale = (256.0 / texture.get_width()) * TEX_SCALE_REF
+	
+	%SinglePourer.scale = Vector2(tex_scale, tex_scale)
+	%PouredIn.scale.x *= TEX_SCALE_REF / tex_scale
+	%PouredIn.scale.y *= TEX_SCALE_REF / tex_scale
 
 func _ready() -> void:
 	%PerfectPourRef.queue_free()
 	
-	poured_in.position.y = UNPOURED
+	poured_in.position.y = unpoured()
 	body_entered.connect(func(body):
 		var delta = 0.025 # Lets us easily test this code in _process(?)
 		pour_percent = min(pour_percent + delta * pour_rate * pour_rate_multiplier, 1.0)
@@ -33,10 +54,10 @@ func _ready() -> void:
 
 func _compute_score() -> void:
 	var y: float = poured_in.position.y
-	done = (y <= POUR_READY)
-	score = 3.999 - 0.2 * abs(y - POUR_PERFECT)
+	done = (y <= pour_ready())
+	score = 3.999 - 0.2 * abs(y - pour_perfect())
 	score = clamp(score, 1.0, 3.999)
 
 func _process(delta: float) -> void:
-	poured_in.position.y = lerp(UNPOURED, POURED, pour_percent)
+	poured_in.position.y = lerp(unpoured(), poured(), pour_percent)
 	_compute_score()
